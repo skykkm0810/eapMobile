2e4982953e949ce2d930218c4d3b42eed84d0180
skykkm0810 <skykkm0810@naver.com>
2021-03-15 19:10:38 +0900

    f

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -3,6 +3,8 @@ import { MatDialog } from '@angular/material/dialog';
 import { PersonalInformationComponent } from '../../modal/personal-information/personal-information.component';
 import { UsageRuleComponent } from '../../modal/usage-rule/usage-rule.component';
 import { LoginComponent} from '../login/login.component';
+import { AuthService } from '../../service/auth.service';
+
 
 @Component({
   selector: 'app-footer',
@@ -15,10 +17,18 @@ export class FooterComponent implements OnInit {
     private dialog:MatDialog,
     private sign_in:MatDialog,
     private dialog2:MatDialog,
+    private auth: AuthService
   ) { }
 
   ngOnInit(): void {
+    this.sign_check();
   }
+
+
+  sign = false;
+  info;
+
+
   pir(){
     const dialogRef = this.dialog.open(PersonalInformationComponent);
 
@@ -29,6 +39,16 @@ export class FooterComponent implements OnInit {
   signin(){
     this.closeBox();
     const dialogRef2 = this.sign_in.open(LoginComponent);
+  }
+  sign_check() {
+    if (!this.auth.isAuthenticated()) {
+      this.sign = false;
+    } else {
+      this.sign = true;
+      // console.log(this.auth.getUserData());
+      this.info = JSON.parse(this.auth.getUserData());
+      console.log(this.info);
+    }
   }
   closeBox(){
     // var box1 = document.getElementsByClassName('searchBox')[0] as HTMLElement;
